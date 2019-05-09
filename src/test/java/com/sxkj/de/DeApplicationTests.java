ced542d67b284651438a0bc699613c8c56954742
NewGr8Player <273221594@qq.com>
2019-05-09 15:15:04 +0800

    Edit:     - 完成动态定时任务

--- src/test/java/com/sxkj/de/DeApplicationTests.java
+++ src/test/java/com/sxkj/de/DeApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,13 +11,6 @@ public class DeApplicationTests {
 
     @Test
     public void contextLoads() {
-
     }
 
-    public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplate();
-        String taskUrl = "http://mobile.weather.com.cn/data/forecast/101010100.html";
-        String result = restTemplate.getForObject(taskUrl, String.class);
-        System.out.println(result);
-    }
 }

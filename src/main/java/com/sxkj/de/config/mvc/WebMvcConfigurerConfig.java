f55fc2b27c2afed996d54bf7dcc523aa4409831b
NewGr8Player <273221594@qq.com>
2019-05-09 10:46:10 +0800

    Edit:     - 修正映射注解

--- src/main/java/com/sxkj/de/config/mvc/WebMvcConfigurerConfig.java
+++ src/main/java/com/sxkj/de/config/mvc/WebMvcConfigurerConfig.java
@@ -1,18 +1,17 @@
 package com.sxkj.de.config.mvc;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.spring.web.SpringfoxWebMvcConfiguration;
 
 /**
  * 注入静态资源
  *
  * @author NewGr8Player
  */
+@EnableWebMvc
 @Configuration
-@ConditionalOnClass(SpringfoxWebMvcConfiguration.class)
 public class WebMvcConfigurerConfig implements WebMvcConfigurer {
 
     @Override
